3c2e5153f3a97a73344476dd3b7cf8f81ff1a221
Charlotte Hennion <chennion@student.otis.edu>
2018-12-03 17:16:27 -0800

    assets

---
+++ p7/lightbox.js
@@ -0,0 +1,28 @@
+// this is a comment
+
+$('header').click(function(){
+    $('header').css('background-color','red');
+    $('h2').html('hello there!');
+})
+
+//lightbox
+$('a').click(function(){
+    //add class of 'clicked' to my link
+    $(this).toggleClass('clicked')
+    
+    // show modal with a fade of .5 seconds
+    $('div.modal').fadeIn(500)
+    
+    //fill content with an image
+    $('div.modal-content').html(
+        $(this).html()
+    )
+    
+    // stop link from going to the next page
+    event.preventDefault()
+})
+
+// close lightbox
+$('div.modal-close').click(function(){
+    $('div.modal').fadeOut(500)
+})
